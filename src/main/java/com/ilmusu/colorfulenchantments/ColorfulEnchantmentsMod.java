960e6d9c311237023f1f68b38ca2317b06cb3242
IlMusu <mattia.musumeci@yahoo.it>
2023-01-09 10:33:51 +0100

    initial commit in which I just set up the modding workspace

---
+++ src/main/java/com/ilmusu/colorfulenchantments/ColorfulEnchantmentsMod.java
@@ -0,0 +1,17 @@
+package com.ilmusu.colorfulenchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class ColorfulEnchantmentsMod implements ModInitializer, ClientModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+	}
+
+	@Override
+	public void onInitializeClient()
+	{
+	}
+}
---
+++ src/main/java/com/ilmusu/colorfulenchantments/Resources.java
@@ -0,0 +1,18 @@
+package com.ilmusu.colorfulenchantments;
+
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Resources
+{
+    public static final String MOD_ID = "colorfulenchantments";
+    public static final String MOD_NAME = "Colorful Enchantments Mod";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static Identifier identifier(String string)
+    {
+        return new Identifier(MOD_ID, string);
+    }
+}
